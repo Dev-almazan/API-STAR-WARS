8ea1b24b403ab2a1db38175401f1358150bd4100
“Dev-almazan” <ernestoalmazanresendiz@gmail.com>
2023-02-08 13:56:26 -0600

    Agregando

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
 const cargando=()=>
 {
 
@@ -8,9 +6,45 @@ const cargando=()=>
         method : 'GET'
     }).then((response)=>
     {
+
+        console.log(response.ok)
+
                 response.json().then((data)=>
                 {
-                    console.log(data.results)
+
+                    let conteo = data.results.length;
+                    
+                    const images = [1,2,3,4,5,6,7,8,9,0]
+
+                    let contenido = document.getElementById('tarjetas');
+
+                        for(a =0 ; a < conteo; a++)
+                        {
+
+
+                            if (data.results[a].cost_in_credits == "unknown")
+                            {
+                                data.results[a].cost_in_credits = 0;
+                            }
+
+                            contenido.innerHTML += 
+                            `
+                                        <div class="col-md-4 mb-5">
+                                            <div class="card " style="width: 100%">
+                                                <img src="img/${images[a]}.jpg" class="card-img-top" alt="..." style="height:300px;">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${data.results[a].name}</h5>
+                                                </div>
+                                                <ul class="list-group list-group-flush">
+                                                    <li class="list-group-item"><b>Model: </b>${data.results[a].model} </li>
+                                                    <li class="list-group-item"><b>Price: </b>${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(data.results[a].cost_in_credits)} </li>
+                                                    <li class="list-group-item"><b>Company: </b>${data.results[a].manufacturer} </li>
+                                                </ul>
+                                            </div>
+                                    </div>
+                            `;
+                           
+                        }
                 })
 
     }).catch((error)=>{
